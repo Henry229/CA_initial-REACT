835ff2fbc3c0ce81445088bb511b055931c365e6
Henry Chun <chunhyungjoon@gmail.com>
2022-12-05 15:10:34 +1100

    initialise REACT

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import Greeting from './Greeting';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Greeting />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <>
+      <p>FR: Bonjour!</p>
+      <p>ES: Hola!</p>
+    </>
+  );
+}
+
+export default Greeting;
